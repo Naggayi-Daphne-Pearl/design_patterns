8e0aec92c61ac7606ab9411b396778e7928a1761
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-26 09:52:32 -0700

    logging in a user

--- singleton.ts
+++ singleton.ts
@@ -3,19 +3,47 @@ class Logger {
     // there should be one and only user at a time 
     // static means that we should be logging in and log out the user
     private static loggedInUser: object;
+    private static userDetail = {}; 
 
+
+    private constructor(){
+        Logger.userDetail = user;
+    }
+    
     // creating a single Instance for a user
     static login(user:object) {
         // we dont use this method, we use the name of the class name
-        if (Logger.loggedInUser === null) {
-            Logger.loggedInUser = new Logger(); // existing logger    
+        if (Logger.loggedInUser != null) {
+            return Logger.loggedInUser;    
         }
-        return Logger.loggedInUser; // calling back the logger
+        Logger.loggedInUser = new Logger(user)
+        
     }
 
+    static getLoggedInUser() {
+        return Logger.userDetail; 
+    }
 
+    static logout() {
+        Logger.loggedInUser = null;  
+    }
 
 }
 
-// creating an instance of Logger
-let logger: Logger = new Logger(); 
+Logger.login({name:'Daphne', password:'12345'}); 
+console.log(Logger.getLoggedInUser());
+Logger.logout(); 
+
+Logger.login({name:'Pearl', password:'12345'}); 
+console.log(Logger.getLoggedInUser());
+Logger.logout(); 
+
+Logger.login({name:'Jackson', password:'12345'}); 
+console.log(Logger.getLoggedInUser());
+
+Logger.login({name:'Sophia', password:'12345'}); 
+console.log(Logger.getLoggedInUser());
+
+
+
+
