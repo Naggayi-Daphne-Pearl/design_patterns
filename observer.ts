57f2ae1bbef15ae32eb764916768fbe13dbfede1
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-29 23:38:03 -0700

    defining the elements in observable design patterns

---
+++ observer.ts
@@ -0,0 +1,28 @@
+// observerable  Class
+    // Pubisher class
+        // 1. State :Object
+        // 2. List of subscribers : Array<Subscriber>
+        // 3. Subscribe(): boolean 
+        // 4. Unsubscribe(): boolean
+        // 5. Notify(): void
+        // 6. Trigger Notifications():void
+        // 7. Publish(state:State): void
+
+// Observer Interface
+    //Subscriber Interface
+        // update(state:State)
+
+
+// Observer Concrete Class 
+    // Subscriber Concrete classe
+
+
+
+
+
+//Testing our code 
+    // Observerable Object(s)
+        // Publisher Object(s)
+
+    // Observer Object(s)
+        // Subscriber Object(s)
