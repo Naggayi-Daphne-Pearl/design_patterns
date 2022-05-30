2019293debc4253f7ae5ac058aabc28249bcf31b
Naggayi Daphne <daphnepearl101@gmail.com>
2022-05-30 00:15:29 -0700

    defining the methods for the NewsPublisher class

--- observer.ts
+++ observer.ts
@@ -1,6 +1,20 @@
+// State 
+    // heading
+    // timeOfPublication
+    // author
+    // content
+
+interface News {
+    timeOfPublication: Date;
+    heading: string;
+    content: string;
+    author: string;
+
+}
+
 // observerable  Class
     // Pubisher class
-        // 1. State :Object
+        // 1. state: State
         // 2. List of subscribers : Array<Subscriber>
         // 3. Subscribe(): boolean 
         // 4. Unsubscribe(): boolean
@@ -8,10 +22,35 @@
         // 6. Trigger Notifications():void
         // 7. Publish(state:State): void
 
+
+class NewsPublisher {
+    // data
+    news: News; 
+    subscribers: Array<Subscriber>;
+
+    // methods 
+    subscriber(subscriber:Subscriber): boolean{
+        return true;
+    }
+    unsubscriber(subscriber:Subscriber): boolean {
+        return true;
+    }
+    notify():void {
+    }
+    triggerNotification():void {
+    }
+    publish():boolean {
+        return true; 
+    }
+}
+
+
 // Observer Interface
     //Subscriber Interface
         // update(state:State)
-
+interface Subscriber {
+    update(news: News): void;
+}
 
 // Observer Concrete Class 
     // Subscriber Concrete classe
